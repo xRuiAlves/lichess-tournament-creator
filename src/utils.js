4fe07c491c4025ddd7a9e5ddcebaf21d342b127f
Rui Alves <up201606746@fe.up.pt>
2020-07-31 09:35:54 +0100

    Add date converting

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 /**
  * 'YYYY-MM-DD hh:mm:ss' Date format regex
  */
-const DATE_FORMAT_REGEX = /^(\d{4}-\d{2}-\d{2}|\d{4}\/\d{2}\/\d{2}) \d{2}:\d{2}:\d{2}$/;
+const DATE_FORMAT_REGEX = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
 
 /**
  * Validates if a number is a non-negative integer
@@ -21,7 +21,23 @@ const isNumberValid = (number) => (
  */
 const isDateFormatValid = (date) => DATE_FORMAT_REGEX.test(date);
 
+/**
+ * Convert 'YYYY-MM-DD hh:mm:ss' date format to epoch-milis format
+ * @param {String} date_str
+ * @throws {Error}
+ * @returns {Integer} Date in epoch format
+ */
+const convertDateFormat = (date_str) => {
+    if (!isDateFormatValid(date_str)) {
+        throw new Error("Invalid input date format.");
+    }
+
+    const [date, time] = date_str.split(" ");
+    return (new Date(`${date}T${time}`)).getTime();
+};
+
 module.exports = {
     isNumberValid,
     isDateFormatValid,
+    convertDateFormat,
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,4 +1,4 @@
-const { isNumberValid, isDateFormatValid } = require("../src/utils");
+const { isNumberValid, isDateFormatValid, convertDateFormat } = require("../src/utils");
 
 describe("Validate numbers in a range", () => {
     it("should validate valid non-negative integers", () => {
@@ -27,10 +27,10 @@ describe("Validate numbers in a range", () => {
 describe("Validate date string format", () => {
     it("should validate valid date strings", () => {
         expect(isDateFormatValid("2019-12-25 06:30:00")).toBe(true);
-        expect(isDateFormatValid("2019/12/25 06:30:00")).toBe(true);
     });
 
     it("should not validate invalid date strings", () => {
+        expect(isDateFormatValid("2019/12/25 06:30:00")).toBe(false);
         expect(isDateFormatValid("2019/12-25 06:30:00")).toBe(false);
         expect(isDateFormatValid("2019-12/25 06:30:00")).toBe(false);
         expect(isDateFormatValid("2019-12-25 06.30.00")).toBe(false);
@@ -38,5 +38,27 @@ describe("Validate date string format", () => {
         expect(isDateFormatValid(" 2019-12-25 06:30:00")).toBe(false);
         expect(isDateFormatValid("2019-12-25 06:30:00 ")).toBe(false);
         expect(isDateFormatValid("2019-12-25  06:30:00")).toBe(false);
+    });
+});
+
+describe("Convert date string", () => {
+    it("should throw when input date format is not valid", () => {
+        expect(
+            () => convertDateFormat(),
+        ).toThrowError("Invalid input date format.");
+
+        expect(
+            () => convertDateFormat("30 July, 2020"),
+        ).toThrowError("Invalid input date format.");
+    });
+
+    it("should convert to epoch-milis format", () => {
+        expect(
+            convertDateFormat("2020-07-30 09:30:15") - ((new Date("2020-07-30T09:30:15")).getTimezoneOffset() * 60 * 1000),
+        ).toBe(1596101415000);
+
+        expect(
+            convertDateFormat("1970-01-01 00:00:00") - ((new Date("1970-01-01T00:00:00")).getTimezoneOffset() * 60 * 1000),
+        ).toBe(0);
     });
 });
